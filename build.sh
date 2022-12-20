eb5a79b21c8776d3606cc9ce8f09e5cc41c69a77
Thijs Eilander <eilander@myguard.nl>
2022-12-20 01:31:39 +0100

    add support for PR's in patchformat

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CDIR=`pwd`
-SRCDIR=`pwd`/src
+SRCDIR="${CDIR}/src"
 
 rm -rf src
 mkdir -p src
@@ -20,17 +20,24 @@ else
     git pull --recurse-submodules
 fi
 
+# Do a hard reset to the last working commit (before bazel got introduced)
 git reset --hard 409bd76
+
 git submodule update --init --recursive --jobs=${NUMCORE} --force
 
-cd ${CDIR}
 
+# Apply some handpicked PR's from https://github.com/apache/incubator-pagespeed-mod/
+for PR in `ls ${CDIR}/pr`
+do
+    patch -p1 < ${CDIR}/pr/${PR}
+done
+
+cd ${CDIR}
 for DIST in trusty xenial bionic focal jammy
 do
     cp docker/Dockerfile-template docker/Dockerfile
     sed -i s/OS/ubuntu-base/ docker/Dockerfile
     sed -i s/DIST/${DIST}/ docker/Dockerfile
-
     cd docker
     docker build --no-cache -t eilandert/psol:${DIST} .
     docker run --volume ${SRCDIR}:/usr/src eilandert/psol:${DIST}
